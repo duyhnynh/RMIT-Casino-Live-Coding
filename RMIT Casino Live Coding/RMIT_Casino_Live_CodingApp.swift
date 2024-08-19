6acc21ccfce47be610bad2e7a79dd2697af15c4b
Duy Huynh <117799318+duyhnynh@users.noreply.github.com>
2024-08-19 16:54:00 +0700

    Initial Commit

---
+++ RMIT Casino Live Coding/RMIT_Casino_Live_CodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_Casino_Live_CodingApp.swift
+//  RMIT Casino Live Coding
+//
+//  Created by Duy Huynh on 19/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_Casino_Live_CodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
